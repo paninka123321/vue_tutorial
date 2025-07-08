764383181d1d4acc73857f7ce6e0adbdcd53b446
paninka123321 <paulinka_k2000@wp.pl>
2025-07-08 22:33:33 +0200

    style-binding

--- mastery_app/main.js
+++ mastery_app/main.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
         description: 'A pair of warm, fuzzy socks.', // Adding a description property
         image: './assets/images/socks_green.jpg', // Adding an image property
         url: 'https://www.vuemastery.com/',
-        onStock: true, // Adding an onStock property
+        onStock: false, // Adding an onStock property
         inventory: 8, // Adding an inventory property
         details: ['50% cotton', '30% wool', '20% polyester'],
         sizes: ['35-38', '39-42', '43-46'], // Adding a sizes property
