c31be0e33df81b21f58aabf97fea46c6c41aa542
paninka123321 <paulinka_k2000@wp.pl>
2025-07-08 13:33:27 +0200

    conditions

--- mastery_app/main.js
+++ mastery_app/main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
         product: 'Socks', // In this place we will store our data
         description: 'A pair of warm, fuzzy socks.', // Adding a description property
         image: './assets/images/socks_green.jpg', // Adding an image property
-        url: 'https://www.vuemastery.com/'
+        url: 'https://www.vuemastery.com/',
+        onStock: true, // Adding an onStock property
+        inventory: 8, // Adding an inventory property
     }
 }
 });
