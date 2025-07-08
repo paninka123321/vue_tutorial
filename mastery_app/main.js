ce63d1c631c453440d7b25c08720eff6e93bdce7
paninka123321 <paulinka_k2000@wp.pl>
2025-07-08 20:40:39 +0200

    Handling events (methods)

--- mastery_app/main.js
+++ mastery_app/main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+        cart: 0,
         product: 'Socks', // In this place we will store our data
         description: 'A pair of warm, fuzzy socks.', // Adding a description property
         image: './assets/images/socks_green.jpg', // Adding an image property
@@ -10,15 +11,22 @@ const app = Vue.createApp({
         details: ['50% cotton', '30% wool', '20% polyester'],
         sizes: ['35-38', '39-42', '43-46'], // Adding a sizes property
         variants: [
-            {
-                id: 2234,
-                color: 'green',
-            },
-            {
-                id: 2235,
-                color: 'blue',
-            }
-        ],
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
     }
+},
+methods: {
+  addToCart() {
+    this.cart += 1 // Incrementing the cart count
+  },
+  removeFromCart() {
+    if (this.cart > 0) {
+      this.cart -= 1 // Decrementing the cart count
+    }
+  },
+  displayColor(variantImage) {
+    this.image = variantImage // Changing the image based on the variant
+  },
 }
 });
